282d1c57f2328ea9bc92c88dc059110cbc84c225
Mahmoud Alhayek <mahmoud.alhayek@plan-b-gmbh.com>
2023-06-28 13:43:25 +0200

    splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+  constructor( private router: Router,) {}
+  async ngOnInit() {
+    await SplashScreen.hide();
+
+    // Show the splash for an indefinite amount of time:
+    // await SplashScreen.show({
+    //   autoHide: false,
+    // });
+
+    // Show the splash for two seconds and then automatically hide it:
+    await SplashScreen.show({
+      showDuration: 2000,
+      autoHide: true,
+    });
+    this.router.navigate(['/login']);
+  }
 }
