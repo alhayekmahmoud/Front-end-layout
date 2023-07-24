aa51820ff9400a12a862f833f346ffa6b3905ec6
Mahmoud Alhayek <mahmoud.alhayek@plan-b-gmbh.com>
2023-07-24 11:52:15 +0200

    header anmaition

--- src/app/pages/mfctemplate/mfctemplate.page.ts
+++ src/app/pages/mfctemplate/mfctemplate.page.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mfctemplate',
@@ -6,8 +7,10 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./mfctemplate.page.scss'],
 })
 export class MfctemplatePage implements OnInit {
+  @ViewChildren('templateList', { read: ElementRef })
+  templateListRef!: QueryList<ElementRef>;
 
-  constructor() { }
+  constructor(private animationCtrl: AnimationController) { }
 
   ngOnInit() {
   }
@@ -15,6 +18,36 @@ export class MfctemplatePage implements OnInit {
     const scrollTop= $event.detail.scrollTop;
     let newPostion= -(scrollTop/5);
     console.log('scrollTop', scrollTop);
+  }
+  templates = [
+    {
+      id: 0,
+      background: 'assets/icon/logo.png',
+      screenPath: 'hotel-booking',
+      text: 'hello'
+    }
+  ];
+  ngAfterViewInit(): void {
+    this.initListAnimation();
+  }
+  ionViewDidEnter(){
+    this.initListAnimation();
+  }
+  initListAnimation() {
+    const itemRefArray = this.templateListRef.toArray();
+    for (let i = 0; i < itemRefArray.length; i++) {
+      const element = itemRefArray[i].nativeElement;
+
+      this.animationCtrl
+        .create()
+        .addElement(element)
+        .duration(1000)
+        .delay(i * (1000 / 3))
+        .easing('cubic-bezier(0.4, 0.0, 0.2, 1.0)')
+        .fromTo('transform', 'translateY(50px)', 'translatey(0px)')
+        .fromTo('opacity', '0', '1')
+        .play();
+    }
   }
   close(){}
 
