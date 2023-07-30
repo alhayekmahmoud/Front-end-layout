97067b5cd4ede2725d62f9be3e42d9d2456b3f1b
Mahmoud Alhayek <mahmoud.alhayek@plan-b-gmbh.com>
2023-07-30 08:54:43 +0200

    chartjs

--- src/app/components/base/cards/report-cards/chart-cards/card1-chart/card1-chart.component.ts
+++ src/app/components/base/cards/report-cards/chart-cards/card1-chart/card1-chart.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  ApexNonAxisChartSeries,
+  ApexPlotOptions,
+  ApexChart,
+  ApexFill,
+  ChartComponent
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  labels: string[];
+  plotOptions: ApexPlotOptions;
+  fill: ApexFill;
+};
 
 @Component({
   selector: 'mfc-card1-chart',
@@ -6,9 +21,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card1-chart.component.scss'],
 })
 export class Card1ChartComponent  implements OnInit {
-
+  public chart: any;
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createChart();
+    // this.createBieChart();
+  }
+
+
+
+  createChart(){
+
+    this.chart = new Chart("MyChart", {
+      type: 'doughnut',
+      data: {
+        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+        datasets: [
+          {
+            label: "Population (millions)",
+            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+            data: [2478,5267,734,784,433]
+          }
+        ]
+      },
+
+  });
+  }
 
 }
+
