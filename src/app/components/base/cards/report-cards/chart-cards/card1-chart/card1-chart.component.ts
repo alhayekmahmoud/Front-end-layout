18d02acf9d6519cb8a81c76dcf4fd54ccf9f7258
Mahmoud Alhayek <mahmoud.alhayek@plan-b-gmbh.com>
2023-07-31 10:17:47 +0200

    chatr1 data

--- src/app/components/base/cards/report-cards/chart-cards/card1-chart/card1-chart.component.ts
+++ src/app/components/base/cards/report-cards/chart-cards/card1-chart/card1-chart.component.ts
@@ -14,6 +14,7 @@ export type ChartOptions = {
   labels: string[];
   plotOptions: ApexPlotOptions;
   fill: ApexFill;
+  colors:string[]
 };
 
 @Component({
@@ -24,53 +25,51 @@ export type ChartOptions = {
 export class Card1ChartComponent  implements OnInit {
 
   @ViewChild("chart") chart!: ChartComponent;
-  public chartOptions: Partial<ChartOptions>;
+  public totalChartOptions!: Partial<ChartOptions>;
+  public clientChartOptions!: Partial<ChartOptions>;
+  public productSoldChartOptions!: Partial<ChartOptions>;
+  public followersChartOptions!: Partial<ChartOptions>;
+
   constructor() {
-    this.chartOptions = {
-      series: [76],
+    this.totalChartOptions = {
       chart: {
+        height: 280,
         type: "radialBar",
-        offsetY: -20
       },
-      plotOptions: {
-        radialBar: {
-          startAngle: -90,
-          endAngle: 90,
-          track: {
-            background: "#e7e7e7",
-            strokeWidth: "97%",
-            margin: 5, // margin is in pixels
-            dropShadow: {
-              enabled: true,
-              top: 2,
-              left: 0,
-              opacity: 0.31,
-              blur: 2
-            }
-          },
-          dataLabels: {
-            name: {
-              show: false
-            },
-            value: {
-              offsetY: -2,
-              fontSize: "22px"
-            }
-          }
-        }
+      series: [67],
+      colors: ["#7dd7ef"],
+      labels: ["Total Orders"]
+
+    };
+    this.clientChartOptions = {
+      chart: {
+        height: 280,
+        type: "radialBar",
       },
-      fill: {
-        type: "gradient",
-        gradient: {
-          shade: "light",
-          shadeIntensity: 0.4,
-          inverseColors: false,
-          opacityFrom: 1,
-          opacityTo: 1,
-          stops: [0, 50, 53, 91]
-        }
+      series: [12],
+      colors: ["#045a65"],
+      labels: ["client"]
+
+    };
+    this.productSoldChartOptions = {
+      chart: {
+        height: 280,
+        type: "radialBar",
       },
-      labels: ["Average Results"]
+      series: [30],
+      colors: ["#6bd098"],
+      labels: ["product Sold"]
+
+    };
+    this.followersChartOptions = {
+      chart: {
+        height: 280,
+        type: "radialBar",
+      },
+      series: [45.9],
+      colors: ["#fbc658"],
+      labels: ["Total Orders"]
+
     };
   }
 
